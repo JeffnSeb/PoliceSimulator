ce9201a606023ab27abfffdf38ea1135f5695123
Seb Free <free.sebastian@gmail.com>
2021-05-09 11:52:19 -0400

    hammy 1

---
+++ main.js
@@ -0,0 +1,21 @@
+// $('.hamburgerIcon').click(function(){
+//     // toggle class show on the menu
+//         // $("nav").toggleClass('show');
+//        $('nav').toggleClass(".show");
+//     //    $(".exitResponsive").toggleClass('show');
+//     });
+//     // $('.exitResponsive').click(function(){
+//     //     $("nav").toggleClass('show');
+//     //    $(".hamburgerNav").toggleClass('show');
+//     //    $(".exitResponsive").toggleClass('show');
+//     // })
+
+$(document).ready(function(){
+    $('ul').click(function(){
+          $('ul').toggleClass('active');
+    })
+})
+
+    
+
+
